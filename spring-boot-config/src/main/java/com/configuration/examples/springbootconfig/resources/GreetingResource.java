c40c09adae296e1d28ce733833081cd9ea3bbfbd
BARAKY <barak.yedaya@gmail.com>
2020-06-05 05:52:42 +0300

    commit

---
+++ spring-boot-config/src/main/java/com/configuration/examples/springbootconfig/resources/GreetingResource.java
@@ -0,0 +1,39 @@
+package com.configuration.examples.springbootconfig.resources;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.configuration.examples.springbootconfig.configuration.DbSettings;
+
+@RestController
+public class GreetingResource {
+
+	@Value("${my.greeting}")
+	private String greetingMessage; //this is the way to configure fields in a property file
+	
+	@Value("${app.description}")
+	private String appDesc;
+	
+	@Value("${app.default: default value}")
+	private String defaultValue; //this is a way to put default value in case the property doesn't exist
+	
+	@Value("${my.list.values}")
+	private List<String> myListValues; //this is a way to configure list comma separated
+	
+	@Value("#{${map.values}}")
+	private Map<String, String> mapValues; 
+	
+	@Autowired
+	private DbSettings dbSettings;
+	
+	@GetMapping("/greeting")
+	public String greeting(){
+		return greetingMessage + " " + appDesc + " " + defaultValue + " " + myListValues + " " + mapValues
+				+ " " + dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
+	}
+}
---
+++ spring-boot-config/src/test/java/com/configuration/examples/springbootconfig/SpringBootConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configuration.examples.springbootconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
